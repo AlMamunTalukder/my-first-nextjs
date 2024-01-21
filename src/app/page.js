fc5e23657e76019baae9e3c9079f75aba94ae6a8
AlMamunTalukder <almamuntalukder0@gmail.com>
2024-01-22 00:58:25 +0600

    learn Server component and client component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Counter from "@/components/Counter/Counter";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>This is next app</h1>
+      <Counter />
     </div>
   );
 }
